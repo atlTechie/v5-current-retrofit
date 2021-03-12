5347b3a0b5a76b07981825e30aced0c76b5b84d8
Erika Wright <hey@ewright.me>
2021-03-12 12:37:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# v5-current-retrofit
+ Retrofitted incentive program participant site base template with theming capability
---
+++ app.js
@@ -0,0 +1,18 @@
+// Bring in Navigation
+$.ajax({
+  url:
+    "https://spreadsheets.google.com/feeds/list/1_BxF8Z3byKpkaYEHLfRkOyCe6n87JGqsmZbpiTOCw8A/od6/public/basic?alt=json",
+  type: "GET",
+  dataType: "json",
+  header: true,
+  success: function (res) {
+    // console.log(res.feed.entry);
+    for (i = 0; i < res.feed.entry.length; i++) {
+      //   console.log(res.feed.entry[i]);
+      var navs = res.feed.entry[i].content.$t;
+      console.log(typeof navs);
+
+      console.log(navs);
+    }
+  },
+});
