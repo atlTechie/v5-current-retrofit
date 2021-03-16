363511506d5d9e5c90fe3ee0e4530652108b268c
Erika Wright <hey@ewright.me>
2021-03-15 21:40:32 -0400

    added item add to cart toast

--- dist/images/RT V5_files/app.js
+++ dist/images/RT V5_files/app.js
@@ -25,21 +25,4 @@ function itemAddedtoCart() {
   var toastHTMLElement = document.getElementById("itemAddedToast");
   var toastElement = new bootstrap.Toast(toastHTMLElement, itemAddedOption);
   toastElement.show();
-
-  document.getElementById("item-added").classList.add = "item-added";
-}
-
-// Item Detail
-
-function itemQtyIncr() {
-  var value = parseInt(document.getElementById("itemQty").value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  document.getElementById("itemQty").value = value;
-}
-function itemQtyDecr() {
-  var value = parseInt(document.getElementById("itemQty").value, 10);
-  value = isNaN(value) ? 0 : value;
-  value--;
-  document.getElementById("itemQty").value = value;
 }
