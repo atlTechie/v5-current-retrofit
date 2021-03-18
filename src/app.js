833309674841682bcfabf47f4aeaa0f9682c13a1
Erika Wright <hey@ewright.me>
2021-03-18 18:04:04 -0400

    updated my-account and claim-detail screens

--- src/app.js
+++ src/app.js
@@ -27,21 +27,28 @@ $.get("../src/data/navigation.json", function (data) {
 });
 
 // Toggle b/w Login-Activation and Forgot Password
-document.getElementById("forgot-password-content").classList.add("hideThis");
 
-document.getElementById("back-to-login").addEventListener("click", function () {
-  //   console.log("clicked");
-  document.getElementById("login-activate-content").style.display = "block";
-  document.getElementById("forgot-password-content").style.display = "none";
-});
+const login = document.getElementById("forgot-password-content");
+login.classList.add("hideThis");
 
-document
-  .getElementById("forgot-your-password")
-  .addEventListener("click", function () {
-    //   console.log("clicked");
-    document.getElementById("login-activate-content").style.display = "none";
-    document.getElementById("forgot-password-content").style.display = "block";
-  });
+if (login) {
+  document
+    .getElementById("back-to-login")
+    .addEventListener("click", function () {
+      //   console.log("clicked");
+      document.getElementById("login-activate-content").style.display = "block";
+      document.getElementById("forgot-password-content").style.display = "none";
+    });
+
+  document
+    .getElementById("forgot-your-password")
+    .addEventListener("click", function () {
+      //   console.log("clicked");
+      document.getElementById("login-activate-content").style.display = "none";
+      document.getElementById("forgot-password-content").style.display =
+        "block";
+    });
+}
 
 /*
 =======================Shopping===============================
